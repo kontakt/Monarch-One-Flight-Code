144a4d23ca4f24445c4d12c22a22956df17715f0
unknown <chuff014@ECED0F0.ts.odu.edu>
2015-03-02 17:13:36 -0500

    Now with basic timing system

--- Monarch-One-Flight-Code.ino
+++ Monarch-One-Flight-Code.ino
@@ -6,11 +6,14 @@
 */ 
 
 /*  CONFIG  */
-// Timings
-const int SD_write       = 0;
-const int solar_read     = 0;
-const int radio_transmit = 0;
-const int sensor_read    = 0;
+// DEBUG
+const bool DEBUG   =   true;
+
+// Timings ( in Microseconds, 1 second = 1,000,000 micros )
+const uint8_t SD_period        = 0;
+const uint8_t solar_period     = 0;
+const uint8_t radio_period     = 0;
+const uint8_t sensor_period    = 0;
 
 // Time values for subroutines
 unsigned long last_time;
@@ -18,6 +21,11 @@ unsigned long SD_time;
 unsigned long solar_time;
 unsigned long radio_time;
 unsigned long sensor_time;
+
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <SD.h>
 
 void setup() {
   // Initialize all hardware here
@@ -29,14 +37,42 @@ void setup() {
   solar_time  =   last_time;
   radio_time  =   last_time;
   sensor_time =   last_time;
+  
+  if(DEBUG) Serial.println(F("Setup Complete"));
 }
 
 void loop() {
   // Main loop
   // Subroutines ranked in order of priority
-  // 
+  if(last_time > solar_time)
+    solar_read();
+  if(last_time > sensor_time)
+    sensor_poll();
+  if(last_time > SD_time)
+    SD_write();
+  if(last_time > radio_time)
+    radio_transmit();
   // Set last loop time
   last_time   =   micros();
 }
 
+/*  SUBROUTINES  */
+void SD_write() {
+  if(DEBUG) Serial.println(F("SD_write"));
+  SD_time      +=   SD_period;
+}
 
+void solar_read(){
+  if(DEBUG) Serial.println(F("Solar_read"));
+  solar_time   +=   solar_period;
+}
+
+void radio_transmit(){
+  if(DEBUG) Serial.println(F("Radio_transmit"));
+  radio_time   +=   radio_period;
+}
+
+void sensor_poll(){
+  if(DEBUG) Serial.println(F("Sensor_poll"));
+  sensor_time  +=   sensor_period;
+}
